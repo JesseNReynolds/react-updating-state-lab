8646bc917af37f678bdfe94706bc36652fee4b07
JesseNReynolds <jessenreynolds@gmail.com>
2021-02-08 12:23:03 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,30 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+    render() {
+        return(
+        <button
+        onClick={() => this.handleClick()}>
+            {this.state.timesClicked}
+        </button>
+        )
+    }
+
+    handleClick() {
+        const newValue = this.state.timesClicked + 1
+        console.log(newValue)
+        this.setState(
+            {timesClicked: newValue}
+        )
+    }
+
+}
+
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,62 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <button
+                className='bitrate'
+                onClick={() => this.bitrate()}>
+                    bitrate
+                </button>
+
+                <button
+                className='resolution'
+                onClick={() => this.resolution()}>
+                    resolution
+                </button>
+            </div>
+        )
+    }
+
+    bitrate() {
+        console.log(this)
+        this.setState({
+            settings: {
+            ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    resolution() {
+        console.log(this)
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    ...this.state.settings.video,
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+}
+
